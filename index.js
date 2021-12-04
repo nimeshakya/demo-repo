223f7bd4325ef47ba1a3ad1af681398092db7a37
nimeshakya <nimeshakyabtw@gmail.com>
2021-12-04 19:33:07 +0545

    updated index.js feature

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello people');
+
+function outPutPyramid(numLines) {
+    let output = '';
+    for (let i = 1; i <= numLines; i++) {
+        for (let j = 0; j < 1; j++) {
+            output += '*';
+        }
+        console.log(output);
+    }
+}
+
+outPutPyramid(5);
